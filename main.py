1d929263b8e4b6a88e06a327668d77b99a0923ab
sanysa <93342013+sanysa@users.noreply.github.com>
2026-07-31 10:31:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telebot import types
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-API_TOKEN = '7244528475:AAFUcVB4Yd5hNH49emg3Rn9CqP_PksEiT64'
+API_TOKEN = os.getenv("7244528475:AAFUcVB4Yd5hNH49emg3Rn9CqP_PksEiT64")
 bot = telebot.TeleBot(API_TOKEN)
 bot.remove_webhook()
 
